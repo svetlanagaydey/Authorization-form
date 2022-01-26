d8a584d3ed44a0416da41721d5618f07ae0a0a54
svetlanagaydey <lana84@ukr.net>
2022-01-26 09:50:13 +0200

    fixing bug

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
+const cors = require("cors")
+const path = require("path")
+
+
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, "../client/build")
+
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors())
+app.use(express.static(publicPath))
 app.use(userRouter);
+
+app.use("*", (req ,res) =>{
+  res.send("didnt find route that match you request")
+})
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
@@ -14,13 +27,13 @@ app.listen(port, () => {
 
 const jwt = require('jsonwebtoken')
 
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
+//     console.log(token)
 
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
+//     const data = jwt.verify(token, 'thisismynewcourse')
+//     console.log(data)
+// }
 //"heroku-postbuild": "npm install --prefix client && npm run build --prefix client",
 
-myFunction()
+// myFunction()
